16f3157bbdd739ad26d98cd437dadc4667fe1221
Harleythetech <jovellano.274348@sanpablo.sti.edu.ph>
2023-12-27 17:34:43 +0800

    Stable  v3 - Rock Paper Scissors C# edition

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Threading;
 
 class Program
 {
+    static string clap = "                    clap                                                 \r\n                                          clap                           \r\n                                                                         \r\n                         Clap      clap                                  \r\n                  clap                                                   \r\n          clap              clap    clap      Clap                       \r\n                      clap                                               \r\n                  clap       clap         clap                           \r\n                          ,         clap                                 \r\n                 clap   clap   Clap        clap                          \r\n           clap                 .  \\  `                                  \r\n                  Clap   \\ ( (\\  ) /                                     \r\n                       `  ` / _\\      ,                                  \r\n                            \\(\")                                         \r\n                  ___    .-  )=|                                         \r\n                 (`  ') ' _  /'|                                         \r\n                 |-n___n '  (/\\|                                         \r\n  a:f____________|_L___J__ <   L _______________________ ";
     static bool gamestat = true;
     static string enter = "Press any key to continue...";
     static string cpuid = Convert.ToString(Microsoft.Win32.Registry.GetValue("HKEY_LOCAL_MACHINE\\HARDWARE\\DESCRIPTION\\SYSTEM\\CentralProcessor\\0", "ProcessorNameString", null)).Trim(); //Remove when using Other OS
@@ -27,17 +28,37 @@ class Program
             {
                 Console.Clear();
                 Console.WriteLine(RPS);
-                Console.WriteLine($"\nPlayer VS CPU ({cpuid}) | SCORE: P1 - {PC} / CPU - {CPU}");
+                Console.WriteLine($"Game History:\nPlayer VS CPU ({cpuid}) | SCORE: P1 - {PC} / CPU - {CPU}");
                 Console.WriteLine($"Player VS Player | SCORE: P1 - {P1} / P2 - {P2}");
                 Console.Write("\nSelect Game Mode:\n1. Player VS CPU \n2. Player VS Player \n3. Exit");
                 Console.Write("\n\nEnter Value: ");
                 if (!int.TryParse(Console.ReadLine(), out modesel) || modesel >= 4)
                 {
                     Console.Clear();
-                    Console.WriteLine($"{RPS}\n{err}");
+                    Console.WriteLine($"{RPS}\n{err}\n{enter}");
                     continue;
                 }
-                else if (modesel == 1)
+                else if (modesel == 3)
+                {
+                    Console.Write("\nAre you Sure You want to Exit? Y/N ");
+                    if (Console.ReadKey().Key == ConsoleKey.Y)
+                    {
+                        //EXIT
+                        Console.Clear();
+                        Console.WriteLine($"{RPS} \nThank You for playing Rock Paper Scissors");
+                        Thread.Sleep(1000);
+                        Environment.Exit(0);
+                    }
+                    else
+                    {
+                        // Lets the user choose another operation
+                        Console.Write($"\n{enter}");
+                        continue;
+                    }
+                }
+
+                //Game Mode 1
+                while (modesel == 1)
                 {
                     // !! REMOVE "{cpuid}" when using other OS, This is a Windows specific thing only !!
 
@@ -58,7 +79,6 @@ class Program
                     Console.WriteLine($"{RPS}\nPlayer VS CPU ({cpuid}) | SCORE: P1 - {PC} / CPU - {CPU}");
                     Console.WriteLine($"\nPlayer Pick: {Pl1}\nCPU Pick: {answer}");//REMOVE AFTER DEBUGGING
 
-
                     //ERROR HANDLING + GAME CONDITIONS [GET CONFUSED] 
                     if ((!string.Equals(Pl1, "Rock", StringComparison.OrdinalIgnoreCase)) && (!string.Equals(Pl1, "Paper", StringComparison.OrdinalIgnoreCase)) && (!string.Equals(Pl1, "Scissors", StringComparison.OrdinalIgnoreCase)))
                     {
@@ -69,29 +89,58 @@ class Program
                     }
                     else if (string.Equals(Pl1, answer, StringComparison.OrdinalIgnoreCase))
                     {
-                        Console.WriteLine("\nIt's a Tie!, Try Again!");
-                        Console.Write($"\n{enter}");
-                        continue;
+                        Console.Clear();
+                        Console.WriteLine($"{RPS}\nPlayer VS CPU ({cpuid}) | SCORE: P1 - {PC} / CPU - {CPU}");
+                        Console.Write($"\n{clap}\n\nIt's a Tie Try Again, Do you want to Continue? Y/N ");
+                        if (Console.ReadKey().Key == ConsoleKey.N)
+                        {
+                            Console.Write("\n"+enter);
+                            break;
+                        }
+                        else
+                        {
+                            continue;
+                        }
                     }
                     else if ((string.Equals(Pl1, "Rock", StringComparison.OrdinalIgnoreCase) && answer == "Scissors") || (string.Equals(Pl1, "Paper", StringComparison.OrdinalIgnoreCase) && answer == "Rock") || (string.Equals(Pl1, "Scissors", StringComparison.OrdinalIgnoreCase) && answer == "Paper"))
                     {
-                        Console.WriteLine("\nYou win!");
+                        Console.Clear();
+                        Console.WriteLine($"{RPS}\nPlayer VS CPU ({cpuid}) | SCORE: P1 - {PC} / CPU - {CPU}");
+                        Console.WriteLine($"\nYou win! \n{clap}");
                         Thread.Sleep(1000);
-                        Console.Write($"\nA point has been added to the player. {enter}");
+                        Console.Write($"\nA point has been added to the player, Do you want to Continue? Y/N ");
                         PC++;
-                        continue;
+                        if (Console.ReadKey().Key == ConsoleKey.N)
+                        {
+                            Console.Write("\n" + enter);
+                            break;
+                        }
+                        else
+                        {
+                            continue;
+                        }
                     }
                     else
                     {
-                        Console.WriteLine("\nCPU win!");
-                        Console.Write($"\nA point has been added to CPU. {enter}");
+                        Console.Clear();
+                        Console.WriteLine($"{RPS}\nPlayer VS CPU ({cpuid}) | SCORE: P1 - {PC} / CPU - {CPU}");
+                        Console.WriteLine($"\nCPU win! \n{clap}");
+                        Console.Write($"\nA point has been added to CPU, Do you want to Continue? Y/N ");
                         CPU++;
-                        continue;
+                        if (Console.ReadKey().Key == ConsoleKey.N)
+                        {
+                            Console.Write("\n" + enter);
+                            break;
+                        }
+                        else
+                        {
+                            continue;
+                        }
                     }
 
-
                 }
-                else if (modesel == 2)
+                // Game Mode 2
+                while (modesel == 2)
                 {
                     Console.Clear();
                     Console.WriteLine($"{RPS}\nPlayer VS Player | SCORE: P1 - {P1} / P2 - {P2}");
@@ -117,7 +166,7 @@ class Program
                         Console.WriteLine($"{RPS}\n{err}");
                         Console.Write($"\n{enter}");
                     }
-                    else if((!string.Equals(Pl2, "Rock", StringComparison.OrdinalIgnoreCase)) && (!string.Equals(Pl2, "Paper", StringComparison.OrdinalIgnoreCase)) && (!string.Equals(Pl2, "Scissors", StringComparison.OrdinalIgnoreCase)))
+                    else if ((!string.Equals(Pl2, "Rock", StringComparison.OrdinalIgnoreCase)) && (!string.Equals(Pl2, "Paper", StringComparison.OrdinalIgnoreCase)) && (!string.Equals(Pl2, "Scissors", StringComparison.OrdinalIgnoreCase)))
                     {
                         Console.Clear();
                         Console.WriteLine($"{RPS}\n{err}");
@@ -125,46 +174,57 @@ class Program
                     }
                     else if (string.Equals(Pl1, Pl2, StringComparison.OrdinalIgnoreCase))
                     {
-                        Console.WriteLine("\nIt's a Tie!, Try Again!");
-                        Console.Write($"\n{enter}");
-                        continue;
+                        Console.Clear();
+                        Console.WriteLine($"{RPS}\nPlayer VS Player | SCORE: P1 - {P1} / P2 - {P2}");
+                        Console.Write($"\n{clap}\n\nIt's a Tie, Try Again. Do you want to Continue? Y/N ");
+                        if (Console.ReadKey().Key == ConsoleKey.N)
+                        {
+                            Console.Write("\n" + enter);
+                            break;
+                        }
+                        else
+                        {
+                            continue;
+                        }
                     }
-                    else if ((string.Equals(Pl1, "Rock", StringComparison.OrdinalIgnoreCase) && (string.Equals(Pl2, "Scissors", StringComparison.OrdinalIgnoreCase)) || (string.Equals(Pl1, "Paper", StringComparison.OrdinalIgnoreCase) && (string.Equals(Pl2, "Rock", StringComparison.OrdinalIgnoreCase))) || (string.Equals(Pl1, "Scissors", StringComparison.OrdinalIgnoreCase) && (string.Equals(Pl1, "Paper", StringComparison.OrdinalIgnoreCase))) ))
+                    else if ((string.Equals(Pl1, "Rock", StringComparison.OrdinalIgnoreCase) && (string.Equals(Pl2, "Scissors", StringComparison.OrdinalIgnoreCase)) || (string.Equals(Pl1, "Paper", StringComparison.OrdinalIgnoreCase) && (string.Equals(Pl2, "Rock", StringComparison.OrdinalIgnoreCase))) || (string.Equals(Pl1, "Scissors", StringComparison.OrdinalIgnoreCase) && (string.Equals(Pl1, "Paper", StringComparison.OrdinalIgnoreCase)))))
                     {
-                        Console.WriteLine("\nPlayer 1 win!");
+                        Console.Clear();
+                        Console.WriteLine($"{RPS}\nPlayer VS Player | SCORE: P1 - {P1} / P2 - {P2}");
+                        Console.WriteLine($"\nPlayer 1 win! \n{clap}");
                         Thread.Sleep(1000);
-                        Console.Write($"\nA point has been added to the player. {enter}");
+                        Console.Write($"\nA point has been added to the P1, Do you want to Continue? Y/N");
                         P1++;
-                        continue;
+                        if (Console.ReadKey().Key == ConsoleKey.N)
+                        {
+                            Console.Write("\n" + enter);
+                            break;
+                        }
+                        else
+                        {
+                            continue;
+                        }
                     }
                     else
                     {
-                        Console.WriteLine("\nPlayer 2 win!");
+                        Console.Clear();
+                        Console.WriteLine($"{RPS}\nPlayer VS Player | SCORE: P1 - {P1} / P2 - {P2}");
+                        Console.WriteLine($"\nPlayer 2 win! \n{clap}");
                         Thread.Sleep(1000);
-                        Console.Write($"\nA point has been added to CPU. {enter}");
+                        Console.Write($"\nA point has been added to P2, Do you want to Continue? Y/N");
                         P2++;
-                        continue;
+                        if (Console.ReadKey().Key == ConsoleKey.N)
+                        {
+                            Console.Write("\n" + enter);
+                            break;
+                        }
+                        else
+                        {
+                            continue;
+                        }
                     }
 
-                }
-                else if (modesel == 3)
-                {
-                    Console.Write("\nAre you Sure You want to Exit? Y/N ");
-                    if (Console.ReadKey().Key == ConsoleKey.Y)
-                    {
-                        //EXIT
-                        Console.Clear();
-                        Console.WriteLine($"{RPS} \nThank You for playing Rock Paper Scissors");
-                        Console.WriteLine(enter);
-                        Thread.Sleep(1000);
-                        Environment.Exit(0);
-                    }
-                    else
-                    {
-                        // Lets the user choose another operation
-                        Console.Write($"\n{enter}");
-                        continue;
-                    }
+                    modesel = 0;
                 }
 
 
