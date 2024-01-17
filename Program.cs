d1ede8254b7558e88df7d3d8e95b9429afc44532
Harleythetech <jovellano.274348@sanpablo.sti.edu.ph>
2024-01-17 16:57:44 +0800

    Fixed Minor Issue V 3.5 rev3

--- Program.cs
+++ Program.cs
@@ -235,7 +235,7 @@ class Program
 
 
 
-            } while (Console.ReadKey().Key != ConsoleKey.Y && modesel != 3);
+            } while (modesel != 3 || Console.ReadKey().Key != ConsoleKey.Y );
         }
         catch (Exception ex)
         {
